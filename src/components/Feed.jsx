5a4eb2682214793884a570771861525a4e7864fb
JARAYTIDA <rajadityakumawat@gmail.com>
2023-05-07 15:16:58 +0530

    done till fetch videos api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,26 @@
 import {useState, useEffect} from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import { Height } from '@mui/icons-material';
+import SideBar from './SideBar';
+import Videos from './Videos';
 
 const Feed = () => {
     return (
         <Stack sx={{flexDirection: {sx: "column", md:"row"}}}>
             <Box sx= {{height:{sx:'auto', md:'93vh'}, borderRight:' 1px solid #3d3d3d', px:{sx:0, md:2}}}>
-                Sidebar
+                <SideBar/>
                 <Typography className='copyright' varient='body2' sx={{mt:1.5, color:"#fff"}}>
                     Copyright 2023 YTube
                 </Typography>
+            </Box>
+
+            <Box p={2} sx={{overflowY:'auto', height:'90vh', flex:2}}>
+                <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}} >
+                    New <span style={{color:'#f31503'}}>
+                        videos
+                    </span>
+                </Typography>
+                <Videos videos = {[]}/>
             </Box>
         </Stack>
     )
