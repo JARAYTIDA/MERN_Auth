68f1624690991b833b3951504a8a91bbf610c1b4
JARAYTIDA <rajadityakumawat@gmail.com>
2023-05-09 13:46:36 +0530

    channel detail page done

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,8 +3,8 @@ import { CheckCircle } from "@mui/icons-material"
 import { Link } from "react-router-dom"
 import { demoProfilePicture } from "../utils/constants"
 
-const ChannelCard = ({channelDetail}) => (
-    <Box sx={{boxShadow:'none', borderRadius:'20px', width:{md:'350px', xs:'450px'}, display:'flex', justifyContent:'center', alignItems:'center', margin:'auto', height:'auto'}}>
+const ChannelCard = ({channelDetail, marginTop}) => (
+    <Box sx={{boxShadow:'none', borderRadius:'20px', width:{md:'350px', xs:'450px'}, display:'flex', justifyContent:'center', alignItems:'center', margin:'auto', height:'auto', marginTop}}>
         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
             <CardContent sx={{display:'flex', flexDirection:'column', justifyContent:'center', textAlign:'center', color:'#fff', }}>
                 <CardMedia
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { Box } from "@mui/material"
+import { Videos, ChannelCard } from "."
+import { FetchFromAPI } from "../utils/FetchFromAPI"
 
 const ChannelDetail = () => {
+    const {id} = useParams();
+    const [channelDetail, setChannelDetail] = useState(null);
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        FetchFromAPI(`channels?parts=snippet&id=${id}`)
+        .then((data) => setChannelDetail(data?.items[0]))
+
+        FetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+        .then((data) => setVideos(data?.items))
+    }, [id])
+
+    console.log(videos);
+
     return (
-        <div>ChannelDetail</div>
+        <Box>
+            <Box>
+                <div
+                    style={{background:'linear-gradient(90deg, rgba(0, 238, 247, 1) 0%, rgba(206, 3, 0, 1) 100%, rgba(0, 212, 255, 1) 100%)', zIndex:10, height:'200px'}}
+                />
+
+                <ChannelCard channelDetail={channelDetail} marginTop='-70px'/>
+            </Box>
+
+            <Box display='flex' p='2'>
+                <Box sx={{mr:{sm:'100px'}, display:'flex', justifyContent:'center', alignItems:'center'}}/>
+                <Videos videos={videos}></Videos>
+            </Box>
+        </Box>
     )
 }
 
